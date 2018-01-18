f5b4eea3cd9492a0e4397912ed87a467f92893c7
Petru Potrimba <you@example.com>
2018-01-18 13:15:29 +0100

    chang database name to "dbfp"

--- Admin/app/ManageMarket/AddItem/php/AddItem.php
+++ Admin/app/ManageMarket/AddItem/php/AddItem.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "testpf";
+$dbname = "dbfp";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
--- Admin/app/ViewOrders/ViewSpecificOrder/SetDelivery/php/SubmitSetDelivery.php
+++ Admin/app/ViewOrders/ViewSpecificOrder/SetDelivery/php/SubmitSetDelivery.php
@@ -2,7 +2,7 @@
 $servername="localhost";
 $username ="root";
 $password ="";
-$database = "testpf";
+$database = "dbfp";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
