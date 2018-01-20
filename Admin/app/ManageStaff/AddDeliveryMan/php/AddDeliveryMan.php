ece436df7719ae94e82982984839db8bf1191a2b
Petru Potrimba <you@example.com>
2018-01-20 21:39:03 +0100

    BUG: update add of delivery man but the submit doesn't work

--- Admin/app/ManageStaff/AddDeliveryMan/php/AddDeliveryMan.php
+++ Admin/app/ManageStaff/AddDeliveryMan/php/AddDeliveryMan.php
@@ -14,22 +14,26 @@ if ($conn->connect_error) {
 }
 
 // prepare and bind
-$stmt = $conn->prepare("INSERT INTO `deliveryman`(`Deleted`, `FiscalCode`, `Name`, `PhoneNumber`, `Surname`) VALUES(?, ?, ?, ?, ?)");
-$stmt->bind_param("sssss", $Deleted, $FiscalCode, $Name, $PhoneNumber, $Surname);
-if(!isset($_POST["name"]) || !isset($_POST["surname"]) || !isset($_POST["fc"]) || !isset($_POST["phone"])) {
+$stmt = $conn->prepare("INSERT INTO `deliveryman` (`Deleted`, `Email`, `FiscalCode`, `Name`, `Password`, `PhoneNumber`, `Surname`) VALUES(?, ?, ?, ?, ?, ?, ?)");
+if(!isset($_POST["name"]) || !isset($_POST["surname"]) || !isset($_POST["fc"]) || !isset($_POST["phone"]) || !isset($_POST["email"])|| !isset($_POST["password"])) {
+  $stmt->bind_param("sssssss", $Deleted, $Email, $FiscalCode, $Name, $Password, $PhoneNumber, $Surname);
   die("Fill all the fields.");
 }
+// if (!mysqli_query($conn,"INSERT INTO `deliveryman` (`Deleted`, `Email`, `FiscalCode`, `Name`, `Password`,`PhoneNumber`, `Surname`) VALUES (?, ?, ?, ?, ?, ?, ?)")) {
+//   echo("Error description: " . mysqli_error($conn));
+// }
 
 $Deleted = "0";
+$Email = $_POST['email'];
 $FiscalCode = $_POST['fc'];
 $Name = $_POST['name'];
+$Password = $_POST['password'];
 $PhoneNumber = $_POST['phone'];
 $Surname = $_POST['surname'];
-$stmt->execute();
 
-echo "New records created successfully";
+$stmt->execute();
 
 $stmt->close();
 $conn->close();
-header("Location: ../html/AddDeliveryMan.html");
+//header("Location: ../html/AddDeliveryMan.html");
 ?>
