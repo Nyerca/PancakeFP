b263daff2864bea04bfdc63018d7df19fd2b74cb
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-01-25 11:19:58 +0100

    implement feature for view all notification (just for adminnotification, TODO also for deliveryman notification

--- Admin/app/WelcomeBoss/php/ViewAllNotification.php
+++ Admin/app/WelcomeBoss/php/ViewAllNotification.php
@@ -1,3 +1,7 @@
+<?php
+ include("connect.php");
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,15 +10,18 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 <meta name="viewport" content="width = device-width, initial-scale = 1">
-<title>Welcome Boss</title>
+
+<title>View all notifications</title>
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" title="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 <link rel="stylesheet" href="../css/WelcomeBoss.css">
+<link rel="stylesheet" type="text/css" title="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 <body>
+
 
 <div class="container">
 
@@ -24,7 +31,7 @@
      <nav class="navbar navbar-inverse">
       <div class="container-fluid">
        <div class="navbar-header">
-        <a class="navbar-brand" href="#">Welcome Boss</a>
+        <a class="navbar-brand" href="#">View all notifications</a>
        </div>
        <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
@@ -32,7 +39,7 @@
          <ul class="dropdown-menu">
            <li>
             <a onclick="" href="#">
-             <strong>View all..</strong><br />
+             <strong>View all notifications</strong><br />
             </a>
            </li>
            <li class="divider"></li>
@@ -43,23 +50,57 @@
      </nav>
 </div>
 
-  <div class="row2">
-    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-      <a href="../../ManageMarket/html/ManageMarket.html" class = "btn btn-default btn-lg" role="button">Manage Market</a>
-    </div>
-    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-      <a href="../../ManageStaff/php/ManageStaff.php" class = "btn btn-default btn-lg" role="button">Manage Staff</a>
-    </div>
-    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-      <a href="../../NewNotification/html/NewNotification.html" class = "btn btn-default btn-lg" role="button">New Notification</a>
-    </div>
-    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-      <a href="../../ViewOrders/html/AllOrders.html" class = "btn btn-default btn-lg" role="button">View Orders</a>
-    </div>
+  <div>
+  	<?php
+
+  			$query_sql="SELECT * FROM adminnotification";
+  			$result = $conn->query($query_sql);
+  			if($result !== false){
+  			?>
+  			<table class="table table-striped">
+  			  <thead>
+  				<tr>
+  				  <th scope="row">Title</th>
+  				  <th scope="row">Description</th>
+  				</tr>
+  			  </thead>
+  			  <tbody>
+  			<?php
+  				if ($result->num_rows > 0) {
+  					while($row = $result->fetch_assoc()) {
+  						?>
+  						<tr onclick="myFunction('<?php echo $row["IDOrder"] ?>')">
+  							<td><?php echo $row["Title"]; ?></td>
+  							<td><?php echo $row["Description"]; ?></td>
+  						</tr>
+  						<?php
+  					}
+  				}
+  			?>
+  			  </tbody>
+  			</table>
+  		  <?php
+  			}
+  			$conn->close();
+  	?>
   </div>
 
 </div>
+<br/>
+<br/>
+  <div class="row2">
+  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <a href="#" class = "btn btn-default btn-lg" role="button">Back</a>
+  </div>
+
+</div>
+
+
 <script type="text/javascript">
+function myFunction() {
+
+}
+
 // var el = document.querySelector('.notification');
 //
 // setInterval(function(){
@@ -129,6 +170,9 @@ function ViewAllNotification() {
     window.location.href ="ViewAllNotification.php";
 }
 
+
 </script>
+
+
 </body>
 </html>
